1708d5c2a1ac1c9150feb5185f4dae89bb7395a0
nada.h <nada.h22425@gmail.com>
2022-05-07 15:33:01 +0200

    notfound page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NotfoundModule } from './componants/notfound/notfound.module';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componants/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  {
+    path: '**',
+    loadChildren: () => import('./componants/notfound/notfound.module').then(m => NotfoundModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/componants/notfound/notfound.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Notfound404Component } from './notfound404/notfound404.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: Notfound404Component }
+];
+
+@NgModule({
+  declarations: [
+    Notfound404Component
+  ],
+  imports: [
+    CommonModule,RouterModule.forChild(routes)
+  ]
+})
+export class NotfoundModule { }
