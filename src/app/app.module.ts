e631edada54de1952a642fff2a2fb5f1d9ddd18c
samaa <samaaehab48@gmail.com>
2022-05-07 02:17:44 +0200

    Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './componants/header/header.component';
+import { HomeComponent } from './componants/home/home.component';
+import { FooterComponent } from './componants/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
