4b768cc571e62f385a2c758b191586fd6db9c39d
samaa <samaaehab48@gmail.com>
2022-05-08 06:09:17 +0200

    course Details

--- src/app/componants/course/course.module.ts
+++ src/app/componants/course/course.module.ts
@@ -4,13 +4,17 @@ import { CoursesComponent } from './courses/courses.component';
 
 
 import { Routes, RouterModule } from '@angular/router';
+import { CourseDetailsComponent } from './course-details/course-details.component';
 
 export const routes: Routes = [
-  { path: '', component: CoursesComponent }
+  { path: '', component: CoursesComponent },
+  { path: 'courseDetails', component: CourseDetailsComponent }
+
 ]
 @NgModule({
   declarations: [
-    CoursesComponent
+    CoursesComponent,
+    CourseDetailsComponent
   ],
   imports: [
     CommonModule,RouterModule.forChild(routes)
