88c7c01d6483e348289f257ec2c161d10640f2c4
nada.h <nada.h22425@gmail.com>
2022-05-13 00:18:26 +0200

    course content

--- src/app/componants/course/course.module.ts
+++ src/app/componants/course/course.module.ts
@@ -6,17 +6,20 @@ import { CoursesComponent } from './courses/courses.component';
 import { Routes, RouterModule } from '@angular/router';
 import { CourseDetailsComponent } from './course-details/course-details.component';
 
- import {MatTabsModule} from '@angular/material/tabs'; 
+ import {MatTabsModule} from '@angular/material/tabs';
+import { CourseContentComponent } from './course-content/course-content.component'; 
 
 export const routes: Routes = [
   { path: '', component: CoursesComponent },
-  { path: 'courseDetails', component: CourseDetailsComponent }
+  { path: 'courseDetails', component: CourseDetailsComponent },
+  { path: 'courseContent', component: CourseContentComponent }
 
 ]
 @NgModule({
   declarations: [
     CoursesComponent,
     CourseDetailsComponent,
+    CourseContentComponent,
   ],
   imports: [
     CommonModule,RouterModule.forChild(routes),MatTabsModule
